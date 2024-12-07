9a41b165346ad7ad882439daaa6cd49e54525279
Darius <darius.hakim@etu.univ-lyon1.fr>
2024-12-07 23:23:30 +0100

    0 eur nouveaux arrivants

--- src/components/Book.js
+++ src/components/Book.js
@@ -50,7 +50,7 @@ function Book() {
                         <div className='bookingInfo'>
                             <div className="price">
                                 <ins>€</ins>
-                                <p>10 EUR</p>
+                                <p>0 EUR</p>
                             </div>
                             <div className="duration">
                                 <ins>◷</ins>
